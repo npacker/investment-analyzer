17c93e8293c3bbba4acf1b2165d879a8fda68a7a
Nigel Packer <packern@wwu.edu>
2022-05-24 04:14:44 +0000

    Fix Not Found controller

--- app/src/Controller/NotFoundController.php
+++ app/src/Controller/NotFoundController.php
@@ -2,14 +2,28 @@
 
 namespace App\Controller;
 
-use App\Http\RequestInterface;
-use App\Http\HttpResponse;
+use App\Container\ContainerInjectionInterface;
+use App\Container\ContainerInterface;
 use App\Controller\AbstractController;
+use App\Controller\ControllerInterface;
+use App\Http\HttpResponse;
+use App\Http\RequestInterface;
+use Twig\Environment as TwigEnvironment;
 
-final class NotFoundController extends AbstractController {
+final class NotFoundController implements ControllerInterface, ContainerInjectionInterface {
+
+  private TwigEnvironment $twig;
+
+  public function __construct(TwigEnvironment $twig) {
+    $this->twig = $twig;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('twig'));
+  }
 
   public function view(RequestInterface $request) {
-    return new HttpResponse($this->render('404.html.twig', [
+    return new HttpResponse($this->twig->render('404.html.twig', [
       'title' => 'Not Found',
     ]), HttpResponse::HTTP_NOT_FOUND);
   }
