6b746a768608727a4a513a12334d16992acdb91b
Nigel Packer <packern@wwu.edu>
2022-05-26 23:31:38 +0000

    Move scripts to footer

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.menu-toggle');
-  const content = document.querySelector('.content');
-  const menu = document.querySelector('.menu');
+const toggle = document.querySelector('.menu-toggle');
+const content = document.querySelector('.content');
+const menu = document.querySelector('.menu');
 
-  toggle.onclick = () => {
-    menu.classList.toggle('hidden');
-    content.classList.toggle('no-menu');
-  };
-});
+toggle.onclick = () => {
+  menu.classList.toggle('hidden');
+  content.classList.toggle('no-menu');
+};
