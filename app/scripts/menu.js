f3ae5dea41df2a244e2c7edeacca1e1a8ccc8661
Nigel Packer <packern@wwu.edu>
2022-05-26 23:28:57 +0000

    Refine menu toggle

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggle = document.querySelector('.menu-toggle');
+  const content = document.querySelector('.content');
   const menu = document.querySelector('.menu');
 
   toggle.onclick = () => {
     menu.classList.toggle('hidden');
+    content.classList.toggle('no-menu');
   };
 });
