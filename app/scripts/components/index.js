2b05200a80dc6438bf497d00e6fb30221df98068
Nigel Packer <packern@wwu.edu>
2022-06-04 22:22:38 +0000

    Export useOnClickOutside

--- app/scripts/components/index.js
+++ app/scripts/components/index.js
@@ -1,2 +1,3 @@
 export { default as ActionMenu } from './ActionMenu';
 export { default as ActionMenuItem } from './ActionMenuItem';
+export { default as useOnClickOutside } from './useOnClickOutside';
