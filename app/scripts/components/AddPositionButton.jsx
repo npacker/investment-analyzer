5fb186a9bb3d22936bec453752e74682b3a00911
Nigel Packer <packern@wwu.edu>
2022-06-05 01:51:41 +0000

    Remove unneeded components

--- app/scripts/components/AddPositionButton.jsx
+++
@@ -1,6 +0,0 @@
-const AddPositionButton = props => {
-
-  return <button type="button" onClick={addPosition}>Add Position</button>
-}
-
-export default AddPositionButton
--- app/scripts/components/NormalizeWeightsButton.jsx
+++
@@ -1,8 +0,0 @@
-const NormalizeWeightsButton = props => {
-  const normalizeWeights = event => {
-  }
-
-  return <button type="button" onClick={normalizeWeights}>Normalize Weights</button>
-}
-
-export default NormalizeWeightsButton
